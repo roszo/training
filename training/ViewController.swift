a71660bc54c2d7313583710b429f7f687b7c7765
Robert Szost <robertszost@Mac-mini-Mac.local>
2017-08-09 17:57:13 +0200

    Initial Commit

---
+++ training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  training
+//
+//  Created by Robert Szost on 09.08.2017.
+//  Copyright © 2017 Robert Szost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
